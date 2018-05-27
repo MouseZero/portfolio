503dada089f8c0e4868395113f08397b3e8dd8a7
MouseZero <rm@mousezero.com>
2018-05-26 17:16:24 -0700

    content for experience section

--- src/home/Experience.jsx
+++ src/home/Experience.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import Section from './partials/Section.jsx'
 import image from '../images/title-backgrounds/code.png'
 
+const style = {
+  dates: {
+    color: "#999",
+    "font-size": "2vw"
+  },
+  title: {
+    "font-weight": "bold"
+  }
+}
+
 function Component(props) {
   return (
     <Section
@@ -9,26 +19,42 @@ function Component(props) {
       image={image}
     >
       <div>
-        Jul 2017 – Present
+        <div style={style.dates}>
+          Jul 2017 – Present
+        </div>
+        <div style={style.title}>
+          Full Stack Developer, VandePoel Productions
+        </div>
+        <div>
+          <ul>
+            <li>
+              Caught and fixed data loss issues on 3 occasions before production release.
+            </li>
+            <li>
+              Was able to find a bug that was stopping 30% of the user base from registering on a pears application. Some content
+            </li>
+          </ul>
+        </div>
+        <div style={style.dates}>
+          Jun 2015 – Jun 2017
+        </div>
+        <div style={style.title}>
+          Full Stack Developer, Redhill Group, Inc.
+        </div>
+        <div>
+          <ul>
+            <li>
+              Had key involvement in the success of an algorithm required to find duplicates in a large data set. This program was paramount to the completion of the project as it could not be solved by brute force alone. A hash table was created to facilitate duplicate detection.
+            </li>
+            <li>
+              In a matter of hours I was able to correct 15,000 unique incorrect links in emails to the wrong  domain.
+            </li>
+            <li>
+              Was able to quickly prototype an application in a language I was not proficient in, by working additional hours. The review was that of a perfectly working program without issue.
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        Full Stack Developer, VandePoel Productions
-      </div>
-      <div>
-        <ul>
-          <li>
-            Caught and fixed data loss issues on 3 occasions before production release.
-          </li>
-          <li>
-            Was able to find a bug that was stopping 30% of the user base from registering on a pears application. Some content
-          </li>
-        </ul>
-      </div>
-      Jun 2015 – Jun 2017
-      Full Stack Developer, Redhill Group, Inc.
-      •	Had key involvement in the success of an algorithm required to find duplicates in a large data set. This program was paramount to the completion of the project as it could not be solved by brute force alone. A hash table was created to facilitate duplicate detection.
-      •	In a matter of hours I was able to correct 15,000 unique incorrect links in emails to the wrong  domain.
-      •	Was able to quickly prototype an application in a language I was not proficient in, by working additional hours. The review was that of a perfectly working program without issue.
     </Section>
   )
 }
