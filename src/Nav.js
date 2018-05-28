2786a94b4c04c347dd55b2079e40cd0b0a87998d
russell-zero <russell@vandepoelproductions.com>
2018-05-28 14:33:35 -0700

    #2 nav bar fits on mobile

--- src/Nav.js
+++ src/Nav.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Icon from '@material-ui/core/Icon';
 import { withRouter } from 'react-router-dom'
 
 const style = {
@@ -14,17 +15,63 @@ const style = {
   tabs: {
   }
 }
+const tabs = [
+  {
+    label: "Cover",
+    icon: (<Icon>star</Icon>),
+    href: "#appBar"
+  },
+  {
+    label: "Experiance",
+    icon: (<Icon>work</Icon>),
+    href: "#experience"
+  },
+  {
+    label: "Portfolio",
+    icon: (<Icon>perm_media</Icon>),
+    href: "#portfolio"
+  },
+  {
+    label: "Skills",
+    icon: (<Icon>build</Icon>),
+    href: "#skills"
+  },
+  {
+    label: "Contact",
+    icon: (<Icon>perm_phone_msg</Icon>),
+    href: "#contact"
+  },
+]
+
+function AdaptingTab({isMobile, icon, label, href, value}) {
+  return (
+    <Tab
+      label={label}
+      icon={(<Icon>{icon}</Icon>)}
+      href={href}
+      value={value}
+    />
+  )
+}
 
 class Header extends React.Component {
-  state = {
-    value: 2,
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 0
+    }
+  }
 
   handleChange = (event, value) => {
     this.setState({ value });
   };
 
   render() {
+    let displayTabs = tabs
+    if (this.props.isMobile) {
+      console.log('gets here')
+      displayTabs = tabs.map(({icon, href}) => ({icon, href}))
+    }
     return (
       <div style={style.content}>
         <Paper style={style.tabs}>
@@ -35,30 +82,13 @@ class Header extends React.Component {
             onChange={this.handleChange}
             centered
           >
-            <Tab
-              label="Cover"
-              href="#appBar"
-            />
-            <Tab
-              label="Experiance"
-              href="#experience"
-            />
-            <Tab
-              label="Portfolio"
-              href="#portfolio"
-            />
-            <Tab
-              label="Skills"
-              href="#skills"
-            />
-            <Tab
-              label="About"
-              href="#about"
-            />
-            <Tab
-              label="Contact"
-              href="#contact"
-            />
+            {
+              displayTabs.map(x => {
+                return (
+                  <Tab {...x}/>
+                )
+              })
+            }
           </Tabs>
         </Paper>
       </div>
